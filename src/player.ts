030e58fa24e49d048b824ba18ae2a24d9319b0f0
Greegko <greegko+dev@gmail.com>
2020-10-04 00:06:52 +0200

    update Hank states

--- src/player.ts
+++ src/player.ts
@@ -2,14 +2,18 @@ import { Sprite } from "pixi.js";
 import { getPlayerSprite } from "./assets";
 import { WALKING_SPEED } from "./globals";
 
+
 export class Player {
   public sprite: Sprite;
+
+  private spriteTextures = getPlayerSprite();
 
   private xDirection: number = 0;
   private yDirection: number = 0;
 
   init(x: number, y: number) {
-    this.sprite = new Sprite(getPlayerSprite().B.texture);
+    this.sprite = new Sprite(this.spriteTextures.F.texture);
+
     this.sprite.x = x;
     this.sprite.y = y;
 
@@ -51,18 +55,22 @@ export class Player {
     switch (key) {
       case 'a':
       case 'ArrowLeft':
+        this.sprite.texture = this.spriteTextures.L.texture;
         this.xDirection = -WALKING_SPEED;
         break;
       case 'w':
       case 'ArrowUp':
+        this.sprite.texture = this.spriteTextures.B.texture;
         this.yDirection = -WALKING_SPEED;
         break;
       case 'd':
       case 'ArrowRight':
+        this.sprite.texture = this.spriteTextures.R.texture;
         this.xDirection = WALKING_SPEED;
         break;
       case 's':
       case 'ArrowDown':
+        this.sprite.texture = this.spriteTextures.F.texture;
         this.yDirection = WALKING_SPEED;
         break;
     }
