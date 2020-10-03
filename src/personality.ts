aef928b767429ef756ba99298106da1e81116684
lsdbianchi <samuele.mercan@gmail.com>
2020-10-03 11:59:09 +0200

    🚧

--- src/personality.ts
+++ src/personality.ts
@@ -1,26 +1,19 @@
 import { Pixi, Globals } from './main'
 
-class Person {
+class Personality {
 
   constructor() {
-    this.sprite = new Pixi.Sprite(
-      Pixi.Resources["t_person"].texture
-    );
 
-    this.sprite.x = Globals.app.renderer.width / 2;
-    this.sprite.y = Globals.app.renderer.height / 2;
 
-    Globals.app.stage.addChild(this.sprite);
 
     
 
   }
 
   update() {
-    this.sprite.x += 1;
   }
 
   
 }
 
-export default Person;
+export default Personality;
