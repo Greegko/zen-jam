1fd665afc750da4f534b1aa4b1ba98e94eecdf43
Greegko <greegko+dev@gmail.com>
2020-10-03 13:12:07 +0200

    add background

--- src/main.ts
+++ src/main.ts
@@ -19,17 +19,10 @@ Loader.shared.add([
 ]).load(setup);
 
 const container = new ParticleContainer();
+const backgroundContainer = new ParticleContainer();
 
 function setup() {
-
-    const tilingSprite = new TilingSprite(
-        RESOURCES["t_ground"].texture,
-        Globals.app.screen.width,
-        Globals.app.screen.height,
-    );
-
-    Globals.app.stage.addChild(tilingSprite);
-
+    generateBackgroundTiles().forEach((tile) => backgroundContainer.addChild(tile));
 
     const player = new Player();
     player.init(100, 100);
@@ -38,14 +31,12 @@ function setup() {
 
     let person = new Person();
     Globals.crowd.push(person);
-    person.init(
-        Globals.app.renderer.width / 2,
-        Globals.app.renderer.height / 2
-    );
+    person.init(100, 100);
 
     container.addChild(player.sprite);
     container.addChild(person.sprite);
 
+    Globals.app.stage.addChild(backgroundContainer);
     Globals.app.stage.addChild(container);
 
     Globals.app.ticker.add(delta => loop(delta));
@@ -67,6 +58,31 @@ function loop(delta) {
 function updatePlayerCamera() {
     const playerSprite = Globals.player.sprite;
     const canvas = document.getElementsByTagName('canvas')[0].getBoundingClientRect();
-    container.x = canvas.width / 2 - playerSprite.x - playerSprite.width / 2;
-    container.y = canvas.height / 2 - playerSprite.y - playerSprite.height / 2;
+    const x = canvas.width / 2 - playerSprite.x - playerSprite.width / 2;
+    const y = canvas.height / 2 - playerSprite.y - playerSprite.height / 2
+
+    container.position.x = backgroundContainer.position.x = x;
+    container.position.y = backgroundContainer.position.y = y;
+}
+
+const createBackgroundTile = (x: number, y: number) => {
+    const tile = new TilingSprite(RESOURCES["t_ground"].texture);
+    tile.x = x;
+    tile.y = y;
+
+    return tile;
+}
+
+function generateBackgroundTiles() {
+    const tiles = [];
+    const width = RESOURCES["t_ground"].texture.width;
+    const height = RESOURCES["t_ground"].texture.height;
+
+    for (let i = -20; i < 20; i++) {
+        for (let k = -20; k < 20; k++) {
+            tiles.push(createBackgroundTile(width * i, height * k));
+        }
+    }
+
+    return tiles;
 }
