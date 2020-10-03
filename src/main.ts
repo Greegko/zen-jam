b177c0f1b85c5c7136cd3545be6299f21401cefb
lsdbianchi <samuele.mercan@gmail.com>
2020-10-03 09:33:21 +0200

    🚧 first pixi setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-document.getElementById('game').innerHTML = 'Hello Zen Jam!';
+// document.getElementById('game').innerHTML = 'Hello Zen Jam!';
+
+import * as PIXI from 'pixi.js'
+
+const app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    // transparent: true 
+});
+document.body.appendChild(app.view);
+
+const bunny = PIXI.Sprite.from('./assets/temp_person.png');
+
+ 
+// Setup the position of the bunny
+bunny.x = app.renderer.width / 2;
+bunny.y = app.renderer.height / 2;
+
+app.stage.addChild(bunny);
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   entry: ["./src/main.ts"],
 
@@ -12,10 +14,26 @@ module.exports = {
     extensions: [".webpack.js", ".web.js", ".js", ".ts", ".tsx"]
   },
 
+  plugins: [
+    new CopyWebpackPlugin({
+       patterns: [
+        { from: 'build/assets', to: 'assets' },
+      ],
+    })
+  ],
+
   module: {
     rules: [{
       test: /\.tsx?$/,
       use: ["ts-loader"]
-    }]
+    },
+    
+    // {
+    //   test: /\.(png|svg|jpg|gif)$/,
+    //   use: [
+    //     'file-loader'
+    //   ]
+    // }, 
+  ]
   }
 };
