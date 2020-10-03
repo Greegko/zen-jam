954eaca2e2ebe52aacbee8fd4d3e02369f8e5dda
lsdbianchi <samuele.mercan@gmail.com>
2020-10-04 01:38:39 +0200

    🚧 testing new asthetic

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Container, Loader, Sprite } from 'pixi.js';
+import { Application, Container, Loader, Sprite, Graphics } from 'pixi.js';
 import Person from './person'
 
 import { Globals } from './globals';
@@ -8,7 +8,8 @@ import { CHARACTER_ASSETS_IDS, getCharacterId, getRandomCharacterSprite } from '
 Globals.app = new Application({
     width: window.innerWidth,
     height: window.innerHeight,
-    backgroundColor: 255 ^ 3 - 255
+    backgroundColor: 255 ^ 3 - 255,
+    
 });
 
 document.body.appendChild(Globals.app.view);
@@ -22,6 +23,13 @@ Loader.shared.add(allAssetsUrls).load(setup);
 
 const container = new Container();
 function setup() {
+
+    const graphics = new Graphics();
+    graphics.beginFill(0x4a4a4a);
+    graphics.drawRect(-1000, -100, 5000, 5000);
+    graphics.endFill();
+    Globals.app.stage.addChild(graphics);
+
     const player = new Player();
     player.init(0, 0);
 
@@ -33,9 +41,9 @@ function setup() {
         Globals.crowd.push(person);
         container.addChild(person.sprite);
     }
-    for (let i = 0; i < 120; i++) {
+    for (let i = 0; i < 500; i++) {
         let person = new Person("lazy");
-        person.init(getRandomCharacterSprite(), Math.random() * (i - 25) * 120, Math.random() * (i - 25) * 120);
+        person.init(getRandomCharacterSprite(), Math.random() * (i - 25) * 10, Math.random() * (i - 25) * 10);
         Globals.crowd.push(person);
         // console.log(person.sprite);
         container.addChild(person.sprite);
@@ -49,7 +57,25 @@ function setup() {
     // }
 
     container.addChild(player.sprite);
+
+
+    
+
+
     Globals.app.stage.addChild(container);
+
+
+
+    let vignette = Sprite.from('./assets/sprites/FXs/vignette.png');
+    vignette.x = 0;
+    vignette.y = 0;
+    vignette.zIndex = -10000;
+    vignette.width =  Globals.app.screen.width;
+    vignette.height =  Globals.app.screen.height;
+    Globals.app.stage.addChild(vignette);
+
+
+
     Globals.app.ticker.add(delta => loop(delta));
 }
 
