01dfc2ec52f1b7cd223e1fe3cd15ad1df4d3b88c
Greegko <greegko+dev@gmail.com>
2020-10-03 23:06:49 +0200

    remove background generation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import { AnimatedSprite, Application, Container, Loader, ParticleContainer, Sprite, Texture, TilingSprite } from 'pixi.js';
+import { AnimatedSprite, Application, Container, Loader, Sprite, Texture } from 'pixi.js';
 import Person from './person'
 
-import { Globals, RESOURCES } from './globals';
+import { Globals } from './globals';
 import { Player } from './player';
-import { hitTestRectangle } from './utils';
 
 Globals.app = new Application({
     width: window.innerWidth,
@@ -46,10 +45,7 @@ Loader.shared.add([
 ]).load(setup);
 
 const container = new Container();
-const backgroundContainer = new ParticleContainer();
 function setup() {
-    generateBackgroundTiles().forEach((tile) => backgroundContainer.addChild(tile));
-
     const player = new Player();
     player.init(getRandomCharacterSprite(), 0, 0);
 
@@ -59,46 +55,33 @@ function setup() {
         let person = new Person();
         person.init(getRandomCharacterSprite(), Math.random() * (i - 25) * 10, Math.random() * (i - 25) * 10);
         Globals.crowd.push(person);
-        // console.log(person.sprite);
         container.addChild(person.sprite);
     }
 
-    // let person = new Person();
-    // person.init(getRandomCharacterSprite(), 300, 300);
-    // Globals.crowd.push(person);
-    // // console.log(person.sprite);
-    // container.addChild(person.sprite);
-
     container.addChild(player.sprite);
-
-    // Globals.app.stage.addChild(backgroundContainer);
     Globals.app.stage.addChild(container);
-
     Globals.app.ticker.add(delta => loop(delta));
 }
-
 
 function loop(delta) {
     Globals.player.update();
 
     updatePlayerCamera();
-    updatePersonsIndex();
 
     for (let i = 0; i < Globals.crowd.length; i++) {
         const person = Globals.crowd[i];
-        if (hitTestRectangle(person.sprite, Globals.player.sprite)) {
-            person.collade();
-        }
 
         person.update(delta, i);
     }
 
+    updatePersonsIndex();
 }
 
 function updatePersonsIndex() {
     const sprites: Sprite[] = [Globals.player.sprite, ...Globals.crowd.map(x => x.sprite)];
-    sprites.sort((x, y) => x.y < y.y ? -1 : 1);
-    sprites.forEach((sprite, index) => container.setChildIndex(sprite, index));
+    sprites
+        .sort((x, y) => x.y !== y.y ? (x.y < y.y ? -1 : 1) : 0)
+        .forEach((sprite, index) => container.setChildIndex(sprite, index));
 }
 
 function updatePlayerCamera() {
@@ -107,28 +90,6 @@ function updatePlayerCamera() {
     const x = canvas.width / 2 - playerSprite.x - playerSprite.width / 2;
     const y = canvas.height / 2 - playerSprite.y - playerSprite.height / 2
 
-    container.position.x = backgroundContainer.position.x = x;
-    container.position.y = backgroundContainer.position.y = y;
-}
-
-const createBackgroundTile = (x: number, y: number) => {
-    const tile = new TilingSprite(RESOURCES["t_ground"].texture);
-    tile.x = x;
-    tile.y = y;
-
-    return tile;
-}
-
-function generateBackgroundTiles() {
-    const tiles = [];
-    const width = RESOURCES["t_ground"].texture.width;
-    const height = RESOURCES["t_ground"].texture.height;
-
-    for (let i = -20; i < 20; i++) {
-        for (let k = -20; k < 20; k++) {
-            tiles.push(createBackgroundTile(width * i, height * k));
-        }
-    }
-
-    return tiles;
+    container.position.x = x;
+    container.position.y = y;
 }
